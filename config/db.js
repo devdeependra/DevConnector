37d215faab62ecdc58d4dfe26bb86de1af5b9370
Deependra Singh <dev.deependra@gmail.com>
2020-09-19 11:37:08 +0530

    Mango DB connection

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });// Returns a promise
+        
+        console.log("Mongo DB connected...");
+    } catch(err) {
+        console.error(err.message);
+        // Exit the process in failure to make application also fail
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
